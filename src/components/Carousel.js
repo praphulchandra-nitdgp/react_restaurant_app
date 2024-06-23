63fe7b69895036604b426949a74ad5ce69b18436
praphulchandra-nitdgp <praphulchandraganapathri1@gmail.com>
2024-06-23 12:27:40 +0530

    text-animation

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -11,6 +11,10 @@ const CustomCarousel = () => {
           src="https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?cs=srgb&dl=pexels-chanwalrus-958545.jpg&fm=jpg"
           alt="Food image"
         />
+        <Carousel.Caption>
+          <h3 className='animation-text'>First Slide</h3>
+          <p className='animation-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
@@ -18,10 +22,10 @@ const CustomCarousel = () => {
           src="https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?cs=srgb&dl=pexels-ella-olsson-572949-1640772.jpg&fm=jpg"
           alt="Second slide"
         />
-        {/* <Carousel.Caption>
-          <h3>Second Slide</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption> */}
+        <Carousel.Caption>
+          <h3 className='animation-text'>Second Slide</h3>
+          <p className='animation-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
@@ -29,6 +33,10 @@ const CustomCarousel = () => {
           src="https://images.indianexpress.com/2023/12/food.jpg"
           alt="Third slide"
         />
+        <Carousel.Caption>
+          <h3 className='animation-text'>Third Slide</h3>
+          <p className='animation-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   );
